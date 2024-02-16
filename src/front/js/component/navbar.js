a010018719353f389b52653911cbc60b1bc9c172
Nicolas Jaramillo <151190011+The-Nick-J@users.noreply.github.com>
2024-02-16 17:02:50 +0000

    navbar mejorada

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -5,34 +5,34 @@ import "../../styles/navbar.css";
 
 export const ChewyNavbar = () => {
   return (
-    <Navbar collapseOnSelect expand="lg" variant="dark" className="border-bottom">
+    <Navbar collapseOnSelect expand="lg" variant="dark" className="custom-navbar">
       <Container>
-        <Navbar.Brand href="/" className="me-2">
+        <Navbar.Brand href="/" className="me-2 custom-brand">
           Mascotienda
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
-            <NavLink to="/Perros" className="nav-link" activeClassName="active">
+            <NavLink to="/Perros" className="nav-link custom-link" activeClassName="active">
               Perros
             </NavLink>
-            <NavLink to="/Gatos" className="nav-link" activeClassName="active">
+            <NavLink to="/Gatos" className="nav-link custom-link" activeClassName="active">
               Gatos
             </NavLink>
-            <NavLink to="/Ofertas" className="nav-link" activeClassName="active">
+            <NavLink to="/Ofertas" className="nav-link custom-link" activeClassName="active">
               Ofertas
             </NavLink>
           </Nav>
           <Nav>
-            <NavLink to="/search" className="nav-link">
-              <i className="fa fa-search" aria-hidden="true"></i>
+            <NavLink to="/search" className="nav-link custom-link">
+              <i className="fa fa-search custom-icon" aria-hidden="true"></i>
             </NavLink>
-            <NavLink to="/cart" className="nav-link">
-              <i className="fa fa-shopping-cart" aria-hidden="true"></i>
+            <NavLink to="/cart" className="nav-link custom-link">
+              <i className="fa fa-shopping-cart custom-icon" aria-hidden="true"></i>
               <span className="cart-count">0</span>
             </NavLink>
-            <NavLink to="/account" className="nav-link">
-              <i className="fa fa-user" aria-hidden="true"></i>
+            <NavLink to="/account" className="nav-link custom-link">
+              <i className="fa fa-user custom-icon" aria-hidden="true"></i>
             </NavLink>
           </Nav>
         </Navbar.Collapse>
