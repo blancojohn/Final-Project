222d81163bb848989cf4ed9cb4827b25412fc890
Nicolas Jaramillo <151190011+The-Nick-J@users.noreply.github.com>
2024-02-16 17:43:12 +0000

    react router dom added

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -7,33 +7,33 @@ export const ChewyNavbar = () => {
   return (
     <Navbar collapseOnSelect expand="lg" variant="dark" className="custom-navbar">
       <Container>
-        <Navbar.Brand href="/" className="me-2 custom-brand">
+        <Navbar.Brand as={NavLink} to="/" className="me-2 custom-brand">
           Mascotienda
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
-            <NavLink to="/Perros" className="nav-link custom-link" activeClassName="active">
+            <Nav.Link as={NavLink} to="/Perros" className="nav-link custom-link" activeClassName="active">
               Perros
-            </NavLink>
-            <NavLink to="/Gatos" className="nav-link custom-link" activeClassName="active">
+            </Nav.Link>
+            <Nav.Link as={NavLink} to="/Gatos" className="nav-link custom-link" activeClassName="active">
               Gatos
-            </NavLink>
-            <NavLink to="/Ofertas" className="nav-link custom-link" activeClassName="active">
+            </Nav.Link>
+            <Nav.Link as={NavLink} to="/Ofertas" className="nav-link custom-link" activeClassName="active">
               Ofertas
-            </NavLink>
+            </Nav.Link>
           </Nav>
           <Nav>
             {/* <NavLink to="/search" className="nav-link custom-link">
               <i className="fa fa-search custom-icon" aria-hidden="true"></i>
             </NavLink> */}
-            <NavLink to="/cart" className="nav-link custom-link">
+            <Nav.Link as={NavLink} to="/cart" className="nav-link custom-link">
               <i className="fa fa-shopping-cart custom-icon" aria-hidden="true"></i>
               <span className="cart-count">0</span>
-            </NavLink>
-            <NavLink to="/account" className="nav-link custom-link">
+            </Nav.Link>
+            <Nav.Link as={NavLink} to="/account" className="nav-link custom-link">
               <i className="fa fa-user custom-icon" aria-hidden="true"></i>
-            </NavLink>
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
