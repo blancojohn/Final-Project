624264d0251b50ea68b39cd86a26554f0fba304c
blancojohn <johnmario.bdc@gmail.com>
2025-12-03 17:43:12 -0300

    Code Splitting para cargas rápidas y diferidas

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -1,23 +1,26 @@
-import React, { useContext } from "react";
+import React, { useContext, lazy, Suspense } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import ScrollToTop from "./component/scrollToTop";
 import { BackendURL } from "./component/backendURL";
-import ProductDetails from "./component/ProductDetails.jsx";
-import Cart from "./component/Cart.jsx";
-import { Home } from "./pages/home";
-import RegisterLogin from "./pages/registerLogin.js";
-import Me from "./component/Me.jsx";
-import { Demo } from "./pages/demo";
-import { Single } from "./pages/single";
+import { ToastContainer } from 'react-toastify';
 import injectContext, { Context } from "./store/appContext";
 
 import { ChewyNavbar } from "./component/navbar";
-import { Footer } from "./component/footer";
-import { ToastContainer } from 'react-toastify';
-import { Categoriap } from "./component/Categoriap.jsx";
-import { Categoriag } from "./component/Categoriag.jsx";
-import { Offer } from "./component/Offer.jsx";
-import ForgotPassword from "./component/ForgotPassword.jsx";
+import { Home } from "./pages/home";
+import { RegisterLogin } from "./pages/registerLogin.js";
+import { Footer } from "./component/footer.js";
+import ScrollToTop from "./component/scrollToTop.js";
+
+// INSTANCIAS QUE NO SON PRIORIDAD DE CARGA INICIAL AL INGRESAR A LA APLICACIÓN
+const Categoriap = lazy(() => import("./component/Categoriap.jsx"));
+const Categoriag = lazy(() => import("./component/Categoriag.jsx"));
+const Offer = lazy(() => import("./component/Offer.jsx"));
+const Single = lazy(() => import("./pages/single"));
+const ProductDetails = lazy(() => import("./component/ProductDetails.jsx"));
+const ForgotPassword = lazy(() => import("./component/ForgotPassword.jsx"));
+const Cart = lazy(() => import("./component/Cart.jsx"));
+const Me = lazy(() => import("./component/Me.jsx"));
+const Demo = lazy(() => import("./pages/demo"));
+
 
 //create your first component
 const Layout = () => {
@@ -31,20 +34,22 @@ const Layout = () => {
             <BrowserRouter basename={basename}>
                 <ScrollToTop>
                     <ChewyNavbar />
-                    <Routes>
-                        <Route element={<Home />} path="/" />
-                        <Route element={<Categoriap />} path="/Perros" />
-                        <Route element={<Categoriag />} path="/Gatos" />
-                        <Route element={<Offer />} path="/Ofertas" />
-                        <Route element={<Demo />} path="/demo" />
-                        <Route element={<Single />} path="/single/:theid" />
-                        <Route element={<ProductDetails />} path="/product/:id" />
-                        <Route element={<RegisterLogin />} path="/register" />
-                        <Route element={<ForgotPassword />} path="/forgotpassword" />
-                        <Route element={<Cart />} path="/cart" />
-                        <Route element={<Me />} path="/me" />
-                        <Route element={<h1>Not found!</h1>} />
-                    </Routes>
+                    <Suspense fallback={<div>Cargando contenido...</div>}>
+                        <Routes>
+                            <Route element={<Home />} path="/" />
+                            <Route element={<Categoriap />} path="/Perros" />
+                            <Route element={<Categoriag />} path="/Gatos" />
+                            <Route element={<Offer />} path="/Ofertas" />
+                            <Route element={<Demo />} path="/demo" />
+                            <Route element={<Single />} path="/single/:theid" />
+                            <Route element={<ProductDetails />} path="/product/:id" />
+                            <Route element={<RegisterLogin />} path="/register" />
+                            <Route element={<ForgotPassword />} path="/forgotpassword" />
+                            <Route element={<Cart />} path="/cart" />
+                            <Route element={<Me />} path="/me" />
+                            <Route element={<h1>Not found!</h1>} />
+                        </Routes>
+                    </Suspense>
                     <ToastContainer />
                     <Footer />
                 </ScrollToTop>
